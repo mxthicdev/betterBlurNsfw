d76dcc2397139ba16b4a097bc1c8bdb6b154afc3
mxthicdev <mxthicpersonal@gmail.com>
2025-07-10 14:29:55 -0700

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,54 @@
+import { Settings } from "@api/Settings";
+import { Devs } from "@utils/constants";
+import definePlugin, { OptionType } from "@utils/types";
+
+let style: HTMLStyleElement;
+
+function setCss() {
+    style.textContent = `
+        .vc-nsfw-img [class^=imageContainer],
+        .vc-nsfw-img [class^=wrapperPaused],
+        .vc-nsfw-img img.emoji,
+        .vc-nsfw-img img[class*=emoji] {
+            filter: blur(${Settings.plugins.BlurNSFW.blurAmount}px);
+            transition: filter 0.2s;
+        }
+        `;
+}
+
+export default definePlugin({
+    name: "BlurNSFW",
+    description: "Blur attachments in NSFW channels until hovered",
+    authors: [Devs.Ven],
+
+    patches: [
+        {
+            find: "}renderEmbeds(",
+            replacement: [{
+                match: /\.container/,
+                replace: "$&+(this.props.channel.nsfw? ' vc-nsfw-img': '')"
+            }]
+        }
+    ],
+
+    options: {
+        blurAmount: {
+            type: OptionType.NUMBER,
+            description: "Blur Amount (in pixels)",
+            default: 90071992547409920,
+            onChange: setCss
+        }
+    },
+
+    start() {
+        style = document.createElement("style");
+        style.id = "VcBlurNsfw";
+        document.head.appendChild(style);
+
+        setCss();
+    },
+
+    stop() {
+        style?.remove();
+    }
+});
